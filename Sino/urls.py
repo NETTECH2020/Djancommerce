feafcacc0c6614dcc05fd7e8f09bbc4f3cef2e8f
NETTECH2020 <89297097+NETTECH2020@users.noreply.github.com>
2021-08-25 18:54:38 +0530

    TRY

--- Sino/urls.py
+++ Sino/urls.py
@@ -14,3 +14,23 @@ urlpatterns = [
 
 ]
 admin.site.site_header = 'Shop Admin'
+
+
+
+from django.urls import path 
+from . import views
+
+
+
+urlpatterns = [
+    path('',views.index,name='home'),
+    path('confirm', views.confirm),
+    path('registration/',views.registrationpage,name='registartion'),
+    path('login/',views.loginpage,name='login'),
+    path('logout/',views.logoutUser, name='logout'),
+    path('atm/',views.atmlogin, name= 'atm-page'),
+    path('bluebox/',views.bluebox,name='bluebox'),
+    path('Workschedule/',views.workSchedule,name='Workschedule'),
+    path('Raise/',views.Raise,name='Raise'),
+
+]
